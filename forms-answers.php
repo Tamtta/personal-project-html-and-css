0fd1dacab49d5227af1206bebb19e66a5e4344d0
TAMTA JOJUA <jojua.tamta99@gmail.com>
2022-05-16 12:14:28 +0400

    almost every component is updated

--- forms-answers.php
+++ forms-answers.php
@@ -1,8 +1,15 @@
-<html>
-<body>
-
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-
-</body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Your answers goes here!</title>
+  </head>
+  <body>
+    <h1>
+      Your question has been successfully submitted to our Admissions Team. You
+      should expect to hear back from us within one business day.
+    </h1>
+  </body>
 </html>
